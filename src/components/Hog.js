1f7763cc3a8789fd0fab677b3384d8bf3dc4930e
Nicole Dow <nikki.dow71@gmail.com>
2019-11-24 09:16:28 -0700

    wip

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 export default class Hog extends React.Component {
-    constructor(hogData) {
-        super()
-        this.state = {}
-    }
+   constructor() {
+       super()
+       this.state = {}
+   }
 
     render() {
         {console.log(this.props.hogData);}
       return (
        <div className="ui eight wide column pigTile">
+         <img src={`../${this.props.hogData.name.split(' ').join('_').toLowerCase()}`} />
          <h4>{this.props.hogData.name}</h4>
        </div>
       )
